e3d8292b20baba13643d3f8d275a997dc95780f5
MatiasCaamal <matiascaamal@hotmail.com>
2025-02-16 22:39:00 -0600

    BoyState

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:pokemon_app/button.dart';
+//import 'package:pokemon_app/button.dart';
+//import 'package:pokemon_app/characters/boy.dart';
+//import 'package:pokemon_app/maps/littleroot.dart';
+import 'button.dart';
+import 'characters/boy.dart';
+import 'maps/littleroot.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,13 +28,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  void moveUp() {}
-  void moveDown() {}
-  void moveRight() {}
-  void moveLeft() {}
+  /*
+    VARIABLES
+
+  */
+
+  // littleroot
+  double mapX = 1.125;
+  double mapY = 0.65;
+
+  // boy character
+  int boySpriteCount = 0;
+  String boyDirection = 'Down';
+
+  // game stuff
+  String currentLocation = 'littleroot';
+
+  double step = 0.25;
+
+  void moveUp() {
+    boyDirection = 'Up';
+    setState(() {
+      mapY += step;
+    });
+    animateWalk();
+  }
+
+  void moveDown() {
+    boyDirection = 'Down';
+    setState(() {
+      mapY -= step;
+    });
+    animateWalk();
+  }
+
+  void moveRight() {
+    boyDirection = 'Right';
+    setState(() {
+      mapX += step;
+    });
+    animateWalk();
+  }
+
+  void moveLeft() {
+    boyDirection = 'Left';
+    setState(() {
+      mapX -= step;
+    });
+    animateWalk();
+  }
+
   void pressedA() {}
   void pressedB() {}
 
+  void animateWalk() {
+    Timer.periodic(Duration(milliseconds: 50), (timer) {
+      setState(() {
+        boySpriteCount++;
+      });
+
+      if (boySpriteCount == 3) {
+        boySpriteCount = 0;
+        timer.cancel();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,11 +103,31 @@ class _HomePageState extends State<HomePage> {
             aspectRatio: 1,
             child: Container(
               color: Colors.black,
+              child: Stack(
+                children: [
+                  // little root
+                  LittleRoot(
+                    x: mapX,
+                    y: mapY,
+                    currentMap: currentLocation,
+                  ),
+
+                  // boy character
+                  Container(
+                    alignment: Alignment(0, 0),
+                    child: MyBoy(
+                      location: currentLocation,
+                      boySpriteCount: boySpriteCount,
+                      direction: boyDirection,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           Expanded(
             child: Container(
-              color: Colors.grey[800],
+              color: Colors.grey[900],
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Column(
